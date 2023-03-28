9f33cdbbf60684c1bedd0e0f4d12d5c2af7f0fa6
JUAN SILVA <54045471+dderking@users.noreply.github.com>
2023-03-28 11:24:53 -0300

    update in requested changes

--- questoes/fibonacci/Solution.java
+++ questoes/fibonacci/Solution.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
