555482767e93456283d78771fa0f0c221076de01
Vandressa galdino <vandressa.soares@ccc.ufcg.edu.br>
2023-03-28 17:46:59 -0300

    finalizando questao: Soma recursiva

---
+++ questoes/add_recursivo/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static int somaRecursiva(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return somaRecursiva(x + 1, y - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] numeros = sc.nextLine().split(" ");
+        int soma = somaRecursiva(Integer.parseInt(numeros[0]), Integer.parseInt(numeros[1]));
+        System.out.println(soma);
+    }
+}
